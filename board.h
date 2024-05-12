89de0e4e7170ac7bfe1b45b5e0cc2d60c289a477
MathieuYolo <mathieu.801@live.fr>
2024-05-12 03:58:46 +0200

    gpg signing on PC perso + header C file

---
+++ board.h
@@ -0,0 +1,10 @@
+void emptyBoard(char *board);
+void printBoard(char *board);
+void initBoard(char *board);
+char get_file(int a);
+char get_rank(int a);
+char* get_coordinates(int a);
+int get_color(char piece);
+char* char_to_string(char a);
+void bishop_move(int pos);
+void pawn_move(int pos);
